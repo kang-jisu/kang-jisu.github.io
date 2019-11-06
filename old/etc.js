7022a19fb09ed0ccbbc9da271b45307a58415701
kang-jisu <rkdwltn0425@gmail.com>
2019-11-06 23:44:26 +0900

    반응형

--- old/etc.js
+++ old/etc.js
@@ -7,17 +7,16 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       var ori = document.getElementById("header").className;
       if(ori!=="undifined"){
-      var cha = ori.replace(' header-top',' header-shrink');
+      var cha = ori.replace('etc-header-top',' header-shrink');
       document.getElementById("header").className = cha;
       }
       
         
   } else {
     var ori = document.getElementById("header").className;
-    var cha = ori.replace(' header-shrink',' header-top');
+    var cha = ori.replace(' header-shrink','etc-header-top');
     var ori = document.getElementById("header").className = cha;
 
 
   }
 }
-
